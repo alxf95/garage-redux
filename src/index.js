3609fa7b2385bdf95f61c81a63d5b2437d28c60b
Alex Fiennes <alexfiennes246@gmail.com>
2020-08-20 12:39:27 +0100

    initial commit for redux garage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import reduxPromise from 'redux-promise';
+import logger from 'redux-logger';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
+const reducers = combineReducers({
+  // key: reducer
+});
+
+const middlewares = applyMiddleware(reduxPromise, logger);
+
+// render an instance of the component in the DOM
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={createStore(reducers, {}, middlewares)}>
+    <BrowserRouter>
+      <Switch>TODO</Switch>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
